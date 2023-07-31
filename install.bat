bc8ba8d8688b2ccffb223d84d8d796bc01b72480
GL4D1AT0R <gyandevrey6@gmail.com>
2023-07-31 19:26:34 -0400

    req and install.bat
    added both based on user requirements.

---
+++ install.bat
@@ -0,0 +1,1 @@
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install nodejs npm pip -y && pip install bs4 pythondns dnspython Levenshtein requests tqdm python-whois urllib3 psutil pandas pycryptodome flask numpy scipy scikit-learn tldextract && npm install express body-parser fs child_process cors && node Server/server.js
