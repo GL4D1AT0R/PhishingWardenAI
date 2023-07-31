e6e11bda10c6f66c8493273535e2f481eb32a31e
GL4D1AT0R <gyandevrey6@gmail.com>
2023-07-31 19:22:33 -0400

    Started the front end to basic server and testing with pipedream.
    Readme needs more details later.

---
+++ Frontend/README.md
@@ -0,0 +1,4 @@
+TO ADD DATA. 
+Basically 
+1. front end of the extension
+2. the second part is sending the url to pipedream to add to the dataset. 
---
+++ Frontend/test.js
@@ -0,0 +1,15 @@
+const headers = new Headers()
+headers.append("Content-Type", "application/json")
+
+const body = {
+  "test": "event"
+}
+
+const options = {
+  method: "POST",
+  headers,
+  mode: "cors",
+  body: JSON.stringify(body),
+}
+
+fetch("https://eofn7ve6epuaruq.m.pipedream.net/", options)
